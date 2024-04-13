8f371c2e9968e7a5aa81ebff58cc940392155f97
Nayaker <nayak.primary@gmail.com>
2024-04-13 14:03:01 +0530

    minor changes

--- client/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './userslice';
-
-export const store = configureStore({
-    reducer: userReducer
-})
-
-  
-export default store;
