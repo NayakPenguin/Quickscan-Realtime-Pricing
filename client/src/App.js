9b32601019d0129ff122f9dfaf382a7367b58324
Nayaker <nayak.primary@gmail.com>
2024-04-13 14:02:22 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
   useEffect(() => {
     const isAuthenticated = () => {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
 
       if (!token) {
         return false;
@@ -18,16 +18,30 @@ const App = () => {
       try {
         const decodedToken = jwtDecode(token);
         console.log(decodedToken);
+
+        const expirationTime = decodedToken.exp * 1000;
+        const expirationDate = new Date(expirationTime);
+        console.log("Token expiration time:", expirationDate.toLocaleString());
+
+        const currentTime = new Date();
+        console.log("Current time:", currentTime.toLocaleString());
+
+
+        // Check if token is expired
+        if (expirationDate < Date.now()) {
+          console.log("Token has expired");
+          return false;
+        }
         return true;
       } catch (error) {
-        console.error('Error decoding JWT:', error);
+        console.error("Error decoding JWT:", error);
         return false;
       }
     };
 
     if (!isAuthenticated()) {
-      // User is not authenticated, redirect to /qrscan/0
-      navigate('/qrscan/0');
+      // User is not authenticated or token has expired, redirect to /qrscan/0
+      navigate("/qrscan/0");
     }
   }, [navigate]);
 
@@ -35,6 +49,7 @@ const App = () => {
     <div>
       <Routes>
         <Route path="/qrscan/:scanId" element={<PreMenu/>} />
+        <Route path="/s" element={<PreMenu/>} />
       </Routes>
     </div>
   )
