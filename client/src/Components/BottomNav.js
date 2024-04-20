f83ee116b1aecd1d95be087f17eac87c07d9f2d4
Nayaker <nayak.primary@gmail.com>
2024-04-20 12:22:20 +0530

    minor girls

--- client/src/Components/BottomNav.js
+++ client/src/Components/BottomNav.js
@@ -12,6 +12,9 @@ import MessageIcon from '@material-ui/icons/Message';
 const BottomNav = () => {
     return (
         <Container>
+            <div className="upper-layer-text">
+                Added 3 items for ₹192.78. <b>Click to Order</b>
+            </div>
             <a href="/" className="link curr-link">
                 <ListIcon/>
                 <div className="text">Menu</div>
@@ -46,13 +49,40 @@ const Container = styled.div`
     
     z-index: 500;
 
-    border-top: 1px solid rgb(233, 229, 229);
+    /* border-top: 1px solid rgb(233, 229, 229); */
     background-color: rgba(255, 255, 255, 1);
-    box-shadow: rgba(0, 0, 0, 0.05) 1px 1px 10px 0px;
+    /* box-shadow: rgba(0, 0, 0, 0.05) 1px 1px 10px 0px; */
 
     display: flex;  
     align-items: flex-start;
     justify-content: space-between;
+
+    .upper-layer-text{
+        position: absolute;
+        height: 38px;
+        top: -38px;
+        font-size: 0.8rem;
+        z-index: 500;
+
+        border-top: 1px solid rgb(233, 229, 229);
+        background-color: #ede76d;
+
+        width: 100%;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        padding: 0 15px;
+        font-weight: 500;
+
+        b{
+            background-color: orange;
+            padding: 2.5px 7.5px;
+            border-radius: 5px;
+        }
+    }
 
     .link{
         display: flex;
@@ -81,7 +111,7 @@ const Container = styled.div`
     }
 
     .curr-link{
-        background-color: #eae9ce;
+        background-color: #fff6f6;
 
         svg{
             fill: #333;
