c5907f74234a6060fb474a2d7f678bd822e0c9e1
Nayaker <nayak.primary@gmail.com>
2024-07-02 18:25:34 +0530

    firebase update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,12 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ0vfULdQFYUJs07XwhbdZQVm3DXl128g",
-  authDomain: "slurp-client.firebaseapp.com",
-  projectId: "slurp-client",
-  storageBucket: "slurp-client.appspot.com",
-  messagingSenderId: "455232250572",
-  appId: "1:455232250572:web:3139330c983470333a5869",
-  measurementId: "G-221BGXC4LL"
+  apiKey: "AIzaSyA7x7gb6vl88tJDbttTF6vYmjKXu3kv_Zw",
+  authDomain: "quickscan-alpha-v1.firebaseapp.com",
+  projectId: "quickscan-alpha-v1",
+  storageBucket: "quickscan-alpha-v1.appspot.com",
+  messagingSenderId: "117587023341",
+  appId: "1:117587023341:web:e172f3f2bb5861b4e5c3e4"
 };
 
 
